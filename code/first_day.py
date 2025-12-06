522a7ce24689884607f6988458d7590c324fedb1
CYZ <530039670@qq.com>
2025-12-06 16:44:38 +0800

    笔记更新-python取整

--- code/first_day.py
+++ code/first_day.py
@@ -1,1 +1,20 @@
-123
+from typing import List
+
+
+def majorityElement(nums: List[int]) -> int:
+        nums.sort()
+        print(nums)
+        n = 0
+        k = 1
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                k = k + 1
+            if k > len(nums)//2:
+                n = nums[i]
+            k = 1
+        return n
+
+
+a = majorityElement(nums=[2,2,1,1,1,2,2])
+
+print(a)
